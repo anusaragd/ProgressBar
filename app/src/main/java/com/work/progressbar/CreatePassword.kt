9ce4a9f922649e2237ca025711cc4dec5dc2195f
anusaragd <anusara.orm@gmail.com>
2023-03-22 16:03:36 +0700

    ีupdate  22/03/2023

--- app/src/main/java/com/work/progressbar/CreatePassword.kt
+++ app/src/main/java/com/work/progressbar/CreatePassword.kt
@@ -1,6 +1,8 @@
 package com.work.progressbar
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import android.widget.CheckBox
 import android.widget.CompoundButton
@@ -13,9 +15,30 @@ class CreatePassword : AppCompatActivity() {
         setContentView(R.layout.activity_create_password)
 
 
-        (findViewById<View>(R.id.createpassword) as PinView).setAnimationEnable(true)
-        (findViewById<View>(R.id.cfpassword) as PinView).setAnimationEnable(true)
+//        (findViewById<View>(R.id.createpassword) as PinView).setAnimationEnable(true)
+//        (findViewById<View>(R.id.cfpassword) as PinView).setAnimationEnable(true)
 
+
+
+//        Handler().postDelayed({
+//            // on below line we are
+//            // creating a new intent
+//            val i = Intent(
+//                this@CreatePassword,
+//                PasswordLogin::class.java
+////            val i = Intent(
+////                this@MainActivity2,
+////                PasswordLogin::class.java
+//
+//            )
+//            // on below line we are
+//            // starting a new activity.
+//            startActivity(i)
+//
+//            // on the below line we are finishing
+//            // our current activity.
+//            finish()
+//        }, 5000)
 
 
     }
--- app/src/main/java/com/work/progressbar/PasswordLogin.kt
+++ app/src/main/java/com/work/progressbar/PasswordLogin.kt
@@ -2,11 +2,18 @@ package com.work.progressbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.chaos.view.PinView
 
 class PasswordLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_password_login)
+
+
+        val pin = findViewById<PinView>(R.id.passwordlogin)
+        pin.setText("")
+
+
 
 
 
