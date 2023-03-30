0148dc71869ede0132e482fd231e9695bcddfbb2
anusaragd <anusara.orm@gmail.com>
2023-03-30 16:53:44 +0700

    ีupdate create Password to login app 30/03/2023

--- app/src/main/java/com/work/progressbar/CreatePassword.kt
+++ app/src/main/java/com/work/progressbar/CreatePassword.kt
@@ -1,19 +1,27 @@
 package com.work.progressbar
 
 import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.chaos.view.PinView
 import com.work.progressbar.databinding.ActivityCreatePasswordBinding
-import com.work.progressbar.databinding.ActivityMainBinding
 
 
 class CreatePassword : AppCompatActivity() {
 
     private lateinit var binding: ActivityCreatePasswordBinding
+
+    private val ler: SQLiteDatabase? = null
+    private  var escrever:SQLiteDatabase? = null
+//    private val helper: SqlHelper? = null
+
+    fun getEscrever(): SQLiteDatabase? {
+        return escrever
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_password)
@@ -62,21 +70,36 @@ class CreatePassword : AppCompatActivity() {
 
 
 
-        (findViewById<View>(R.id.createpassword) as PinView).setAnimationEnable(true)
-        (findViewById<View>(R.id.cfpassword) as PinView).setAnimationEnable(true)
+//        binding.ctpassword = (findViewById<View>(R.id.createpassword) as PinView).setAnimationEnable(true)
+//        binding.cfpassword = (findViewById<View>(R.id.cfpassword) as PinView).setAnimationEnable(true)
+//
+//        binding.createpassword.setText()
+
+        val pinview1 = findViewById<PinView>(R.id.createpassword)
+        pinview1.setText("")
+        pinview1.setPinViewEventListener(object : PinViewEventListener {
+
+            override fun onDataEntered(pinview: PinView?, fromUser: Boolean) {
+                Toast.makeText(this@CreatePassword, pinview!!.value, Toast.LENGTH_SHORT).show()
+            }
+        })
 
 
-//        et_username = (EditText)findViewById(R.id.et_username);
-//        et_password = (EditText)findViewById(R.id.et_password);
 
 //        btn_login.setOnClickListener(View.OnClickListener {
 //            if (et_username.getText().toString().equals("admin") && et_password.getText().toString()
-//                    .equals("admin")
-//            ) {
-                Toast.makeText(this@CreatePassword, "Username and Password is correct", Toast.LENGTH_SHORT)
+            if (PinView.getText().toString().equals("admin") && PinView.getText().toString()
+                    .equals("admin")
+            ) {
+                Toast.makeText(
+                    this@CreatePassword,
+                    "Username and Password is correct",
+                    Toast.LENGTH_SHORT
+                )
                     .show()
                 val intent = Intent(this@CreatePassword, AddressActivity::class.java)
                 startActivity(intent)
+            }
 //            } else {
 //                Toast.makeText(this@CreatePassword, "Username or Password is incorrect", Toast.LENGTH_SHORT)
 //                    .show()
